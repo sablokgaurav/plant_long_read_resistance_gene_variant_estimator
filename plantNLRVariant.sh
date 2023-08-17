aaa871eaabe68d98de6f1bb1abef7600d1c219e9
Gaurav Sablok <sablokg@gmail.com>
2023-08-17 21:06:26 +0530

    Create plantNLRVariant.sh

---
+++ plantNLRVariant.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+# Author Gaurav Sablok
+# MIT License
+# Copyright (c)
+echo "This is a complete workflow for the identification and the analysis of the
+plant NBS LRR genes and it allows for the identification of the NBS LRR genes through
+the comparative mapping approach and also allows for the identification of the
+variants in the plant NBS LRR genes"
+
+echo "This workflow doesnt allow for the consensus construction and also doesnt
+perform the cleaning and if you want to see that feature fork and drop a comment
+and i will add that feature"
+
+echo "Please select the type of the long read assembler that you want to use for
+the long read assembler"
+
+# how to run, download this file and then run the file as the sh plantNLRVariant.sh 
